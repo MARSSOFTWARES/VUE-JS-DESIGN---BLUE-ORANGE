c4e0c95257fc53c67f33ae78d0e069a0c4d63e5d
MARSSOFTWARES <102754255+MARSSOFTWARES@users.noreply.github.com>
2022-05-11 20:26:02 +0200

    Add files via upload

---
+++ src/components/global/AppFooter.vue
@@ -0,0 +1,140 @@
+<template>
+    <footer class="text-center text-lg-start text-muted">
+          <!-- Section: Social media -->
+          <section
+            class="
+              d-flex
+              justify-content-center justify-content-lg-between
+              p-4
+              border-bottom
+            "
+          >
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+            <!-- Left -->
+
+            <!-- Right -->
+            <div>
+              <a href="" class="me-4 text-reset">
+                <img src="../../icons/facebook.png" alt="" />
+              </a>
+              <a href="" class="me-4 text-reset">
+                <img src="../../icons/twitter.png" alt="" />
+              </a>
+              <a href="" class="me-4 text-reset">
+                <img src="../../icons/gmail.png" alt="" />
+              </a>
+              <a href="" class="me-4 text-reset">
+                <img src="../../icons/instagram.png" alt="" />
+              </a>
+              <a href="" class="me-4 text-reset">
+                <img src="../../icons/linkedin.png" alt="" />
+              </a>
+              <a href="" class="me-4 text-reset">
+                <img src="../../icons/github.png" alt="" />
+              </a>
+            </div>
+            <!-- Right -->
+          </section>
+          <!-- Section: Social media -->
+
+          <!-- Section: Links  -->
+          <section class="second-section">
+            <div class="container text-center text-md-start mt-5">
+              <!-- Grid row -->
+              <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <!-- Content -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    <img src="../../icons/diamond.png" alt="" /> Blue<span>orange</span>
+                  </h6>
+                  <p>
+                    Here you can use rows and columns to organize your footer
+                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit.
+                  </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+                  <p>
+                    <a href="#!" class="text-reset">Angular</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">React</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Vue</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Laravel</a>
+                  </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <p>
+                    <a href="#!" class="text-reset">Pricing</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Settings</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Orders</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Help</a>
+                  </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p>
+                    <img src="../../icons/home.png" alt="" /> New York, NY 10012,
+                    US
+                  </p>
+                  <p>
+                    <img src="../../icons/envelope.png" alt="" /> info@example.com
+                  </p>
+                  <p><img src="../../icons/phone.png" alt="" /> + 01 234 567 88</p>
+                  <p>
+                    <img src="../../icons/printing.png" alt="" /> + 01 234 567 89
+                  </p>
+                </div>
+                <!-- Grid column -->
+              </div>
+              <!-- Grid row -->
+            </div>
+          </section>
+          <!-- Section: Links  -->
+
+          <!-- Copyright -->
+          <div
+            class="text-center p-4 copy-right"
+          >
+            © 2021 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/"
+              >MDBootstrap.com</a
+            >
+          </div>
+          <!-- Copyright -->
+        </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
